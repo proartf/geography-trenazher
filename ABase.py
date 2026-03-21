ce7690347871f1c9500205bc7f2a26de737da14d
Artem <proartf@mail.ru>
2026-03-21 18:06:50 +0300

    Update ABase.py

--- ABase.py
+++ ABase.py
@@ -1,96 +1,101 @@
-import tkinter as tk
-from tkinter import messagebox
-from PIL import Image, ImageTk
+import customtkinter as ctk
+from PIL import Image
 import random
-import time
-import subprocess
-import os
-import winsound
+import io
+import requests
+from Flags import FLAGS
 
-class Windows11FinalFix:
-    def __init__(self, root):
-        self.root = root
-        self.root.attributes('-fullscreen', True)
-        self.root.configure(bg="#0078d7")
-        self.root.config(cursor="none") 
+ctk.set_appearance_mode("dark")
 
-        # --- BSOD ИНТЕРФЕЙС ---
-        self.main_frame = tk.Frame(self.root, bg="#0078d7")
-        self.main_frame.place(relx=0.1, rely=0.2, anchor="nw")
 
-        tk.Label(self.main_frame, text=":(", font=("Segoe UI", 120), bg="#0078d7", fg="white").pack(anchor="w")
-        tk.Label(self.main_frame, text="Your PC ran into a problem and needs to restart...", 
-                 font=("Segoe UI", 22), bg="#0078d7", fg="white", justify="left").pack(anchor="w", pady=20)
+class FlagQuiz(ctk.CTk):
+    score = 0
 
-        self.progress_val = 0
-        self.perc_lbl = tk.Label(self.main_frame, text="0% complete", font=("Segoe UI", 24), bg="#0078d7", fg="white")
-        self.perc_lbl.pack(anchor="w", pady=10)
+    def __init__(self):
+        super().__init__()
 
-        # QR-код
-        self.qr_canvas = tk.Canvas(self.main_frame, width=100, height=100, bg="white", highlightthickness=0)
-        self.qr_canvas.pack(anchor="w", pady=20)
-        self.generate_qr()
+        self.title("AGeography - Тренажер флагов")
+        self.geometry("500x450")
+        self.data = FLAGS
+        self.current_question = {}
+        self.flag_label = ctk.CTkLabel(self, text="Загрузка...")
+        self.flag_label.pack(pady=20)
+        self.buttons = []
 
-        self.update_progress()
+        for i in range(3):
+            btn = ctk.CTkButton(
+                self, text=f"Вариант {i+1}", command=lambda b=i: self.check_answer(b)
+            )
+            btn.pack(pady=5)
+            self.buttons.append(btn)
 
-    def generate_qr(self):
-        for _ in range(300):
-            x, y = random.randint(0, 100), random.randint(0, 100)
-            self.qr_canvas.create_rectangle(x, y, x+5, y+5, fill="black", outline="")
+        self.score_counter = ctk.CTkLabel(self, text="Игра еще не начата...")
+        self.score_counter.pack()
 
-    def update_progress(self):
-        if self.progress_val < 100:
-            self.progress_val += random.randint(10, 25)
-            if self.progress_val > 100: self.progress_val = 100
-            self.perc_lbl.config(text=f"{self.progress_val}% complete")
-            self.root.after(random.randint(400, 1000), self.update_progress)
+        self.next_question()
+
+    def next_question(self):
+        self.current_question = random.choice(self.data)
+        img_source = self.current_question["image"]
+
+        try:
+
+            if img_source.startswith("http"):
+                response = requests.get(img_source)
+                raw_data = io.BytesIO(response.content)
+                img = Image.open(raw_data)
+            else:
+                img = Image.open(img_source)
+
+            my_image = ctk.CTkImage(light_image=img, size=(250, 150))
+            self.flag_label.configure(image=my_image, text="")
+            self.flag_label.image = my_image
+
+        except Exception as e:
+            self.flag_label.configure(image=None, text="Не удалось загрузить флаг")
+
+        correct_answer = self.current_question["name"]
+        others = [d["name"] for d in self.data if d["name"] != correct_answer]
+        options = random.sample(others, 2) + [correct_answer]
+        random.shuffle(options)
+
+        for i in range(3):
+            self.buttons[i].configure(text=options[i], fg_color="#1f538d")
+
+        self.score_counter.configure(text=self.score_label_text())
+
+    def check_answer(self, btn_index):
+        selected = self.buttons[btn_index].cget("text")
+        if selected == self.current_question["name"]:
+            self.buttons[btn_index].configure(fg_color="green")
+            self.score += 1
+            self.after(1000, self.next_question)
         else:
-            self.root.after(1000, self.fade_out)
+            self.score = 0
+            self.score_counter.configure(text=self.score_label_text())
+            self.buttons[btn_index].configure(fg_color="red")
 
-    def fade_out(self):
-        # Плавное исчезновение
-        alpha = self.root.attributes("-alpha")
-        if alpha > 0.1:
-            self.root.attributes("-alpha", alpha - 0.1)
-            self.root.after(50, self.fade_out)
-        else:
-            self.show_vbs_and_gnome()
+    def get_payer_rank(self) -> str:
+        if self.score < 5:
+            return "Новичок"
+        if self.score < 10:
+            return "Начинающий"
+        if self.score < 15:
+            return "Энтузиаст"
+        if self.score < 20:
+            return "Знаток"
+        return "Легенда"
 
-    def show_vbs_and_gnome(self):
-        # 1. Системное окно
-        vbs = "msg.vbs"
-        with open(vbs, "w", encoding="cp1251") as f:
-            f.write('MsgBox "Ну ты и дурак, вот нефиг было качать что попало с рандомных сайтов!", 48, "System Security"')
-        
-        subprocess.run(["wscript.exe", vbs])
-        
-        # 2. Показываем гриба
-        self.root.attributes("-alpha", 1.0)
-        self.root.configure(bg="black")
-        self.main_frame.destroy()
+    def score_label_text(self) -> str:
+        return (
+            "Правильных ответов подряд: "
+            + str(self.score)
+            + "\n"
+            + "Твой ранг: "
+            + self.get_payer_rank()
+        )
 
-        # Путь к картинке
-        img_path = "prostofilya.jfif"
-        
-        if os.path.exists(img_path):
-            try:
-                img = Image.open(img_path)
-                sw, sh = self.root.winfo_screenwidth(), self.root.winfo_screenheight()
-                img = img.resize((sw, sh), Image.Resampling.LANCZOS)
-                self.photo = ImageTk.PhotoImage(img)
-                tk.Label(self.root, image=self.photo, bg="black").place(relx=0.5, rely=0.5, anchor="center")
-                winsound.MessageBeep(winsound.MB_ICONHAND)
-            except Exception as e:
-                tk.Label(self.root, text=f"Ошибка картинки: {e}", fg="white", bg="black").pack()
-        else:
-            tk.Label(self.root, text="ГДЕ ГРИБ? Положи prostofilya.jfif в папку!", 
-                     font=("Arial", 30), fg="red", bg="black").place(relx=0.5, rely=0.5, anchor="center")
-
-        if os.path.exists(vbs): os.remove(vbs)
-        self.root.after(5000, self.root.destroy)
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.bind("<Escape>", lambda e: root.destroy())
-    app = Windows11FinalFix(root)
-    root.mainloop()
+    app = FlagQuiz()
+    app.mainloop()
